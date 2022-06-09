b11e835d4d8f8e9c50d12964a0fe2dce6779aa6e
Amaan Khilji <56731560+amaankhilji@users.noreply.github.com>
2022-06-09 13:21:49 +0530

    Add files via upload

---
+++ set Matrix Zeroes.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+// typedef long long ll;
+// #define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+// #define REP(i, j) FOR(i, 0, j, 1)
+// typedef std::vector<ll> vi;
+// typedef pair<ll, ll> pi;
+// #define ull unsigned long long
+// #define F first
+// #define S second
+// #define PB push_back
+// #define MP make_pair
+// #define boht_tez ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+// //#define MAXN ll(1e5 + 123)
+// #define MAXINP ll (1e3 + 123)
+// #define MAX_SIZE 100000000
+// #define    DOD(i)  cout<<setprecision(i)<<fixed<<
+// #define MOD 1000000007ll
+// #define pi 3.1415926536
+
+//////////////////
+#define endl "\n"
+#define ll long long
+#define ff first
+#define ss second
+#define vc vector<ll>
+#define vcc vector<vector<ll>>
+#define vp vector<pair<ll,ll>>
+#define pb(a) push_back(a)
+#define mp(a,b) make_pair(a,b)
+#define ist(a) insert(a)
+#define fr(i,a,n) for(ll i=a;i<n;i++)
+#define fr2(i,n,a) for(ll i=n;i>=a;i--)
+#define db1(x)  cerr <<#x<<"="<<x<<'\n'
+#define db2(x,y)  cerr <<#x<<"="<<x<<" , "<<#y<<"="<<y<<'\n'
+#define db3(x,y,z)  cerr <<#x<<"="<<x<<" , "<<#y<<"="<<y<<" , "<<#z<<"="<<z<<'\n'
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+	// for getting input from input.tx
+	freopen("input1.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+	// boht_tez;
+
+	ll tt = 1;
+	cin >> tt;
+	while (tt--) {
+
+
+
+	}
+
+
+
+
+
+}
